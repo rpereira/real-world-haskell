a8ffb5c9843282a453e967b1eeaf539b23183d6f
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2017-06-19 20:40:18 +0100

    Generate a random subsequence of the given list

--- src/Testing/Gen.hs
+++ src/Testing/Gen.hs
@@ -1,6 +1,6 @@
 module Testing.Gen where
 
-import Control.Monad   (replicateM)
+import Control.Monad   (filterM, replicateM)
 import Test.QuickCheck
 
 -- | Generates a list of random length. The maximum length depends on the size
@@ -76,3 +76,21 @@ infiniteListOf' gen = sequence (repeat gen)
 oneOf' :: [Gen a] -> Gen a
 oneOf' [] = error "Cannot be used with empty list"
 oneOf' gs = choose (0, length gs - 1) >>= (gs !!)
+
+-- | Generates a random subsequence of the given list.
+--
+-- Usage example:
+--     > sample (subListOf' [1..10])
+--     [1,2,6,7,8,10]
+--     [1,4,7,8,10]
+--     [1,2,3,5,6,7]
+--     [3,4,6,7,9,10]
+--     [1,2,3,6,7,8,10]
+--     [2,4,6,7,9,10]
+--     [1,2,5,8]
+--     [2,3,4,5,10]
+--     [1,4,5,8,9,10]
+--     [1,7,8,10]
+--     [1,2,3,4,5,7,8,9,10]
+subListOf' :: [a] -> Gen [a]
+subListOf' xs = filterM (\_ -> choose (False, True)) xs
